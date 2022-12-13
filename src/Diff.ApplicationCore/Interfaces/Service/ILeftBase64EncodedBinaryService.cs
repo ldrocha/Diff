0d6642858245a6a374eda082ebaccbdfb118f083
Lorena <lorena@Sashas-MacBook-Pro.local>
2022-12-13 05:56:03 +0000

    Remocao de arquivo desnecessario

---
+++ src/Diff.ApplicationCore/Interfaces/Service/ILeftBase64EncodedBinaryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Diff.ApplicationCore.Requests;
+using Diff.ApplicationCore.Responses;
+
+namespace Diff.ApplicationCore.Interfaces.Services
+{
+	public interface ILeftBase64EncodedBinaryService
+	{
+        LeftBase64EncodedBinaryResponse Get(string id);
+
+        LeftBase64EncodedBinaryResponse AddOrUpdate(LeftBase64EncodedBinaryRequest request);
+    }
+}
